4bc5388e8f1df9a9cf957f4d799c341778d32b5e
Fayaz Ahmed <15716057+fayazara@users.noreply.github.com>
2021-09-18 23:52:17 +0530

    Fixed the v2 badge name

--- components/common/new-version-preview.vue
+++ components/common/new-version-preview.vue
@@ -2,7 +2,7 @@
   <a
     href="https://fluenticons.pages.dev/"
     target="_blank"
-    class="fixed bottom-4 left-4 btn-focus rounded-lg bg-primary dark:bg-gray-700 shadow-lg border z-50 px-4 py-2 text-sm"
+    class="fixed bottom-4 left-4 btn-focus rounded-lg bg-primary shadow-lg border z-50 px-4 py-2 text-sm"
   >
     Here's a look at v2
   </a>
--- pages/index.vue
+++ pages/index.vue
@@ -6,7 +6,7 @@
     <transition name="scroll" mode="out-in">
       <scroll-up v-if="show" />
     </transition>
-    <v2-badge />
+    <new-version-preview />
   </main>
 </template>
 
