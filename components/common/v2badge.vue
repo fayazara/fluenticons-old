4e2d8d97b54e53f57cbfd5d8ac799349e5a6c576
Fayaz Ahmed <15716057+fayazara@users.noreply.github.com>
2021-09-18 23:38:12 +0530

    v2 badge test

---
+++ components/common/v2badge.vue
@@ -0,0 +1,9 @@
+<template>
+  <a
+    href="https://fluenticons.pages.dev/"
+    target="_blank"
+    class="fixed bottom-4 left-4 btn-focus rounded-lg bg-primary dark:bg-gray-700 shadow-lg border z-50 px-4 py-2 text-sm"
+  >
+    Here's a look at v2
+  </a>
+</template>
